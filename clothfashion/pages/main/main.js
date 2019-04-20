2b6cc931151fb58b50a5e8c182e59fd13c111278
masite <1003793102@qq.com>
2019-04-20 12:42:19 +0800

    优化首页界面

--- clothfashion/pages/main/main.js
+++ clothfashion/pages/main/main.js
@@ -1,184 +1,208 @@
 // pages/main/main.js
+var datasource = require('../../utils/data')
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    imageUrl: 'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=776257843,1161785733&fm=27&gp=0.jpg',
-    weather:'晴空万里，旭旭和风',
-    mode: "scaleToFill",
-    arr: [],
-
     // 配置banner
     indicatorDots: true,
     autoplay: true,
     interval: 2000,
     duration: 1000,
 
+    //clothType:1,2,3 上、下、配饰  season 1、2、3、4：春、夏、秋、冬
+    filter: {
+      clothType: 1,
+      season: 1
+    },
+
+    // 轮播图
     bannerdata: {
-      name: '', 
-      urls:[]
+      name: '',
+      urls: []
     },
+
     // 列表数据
-    mydata: [{
-      id: 1,
-      name: '1-1-1',
-      url: ['https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=776257843,1161785733&fm=27&gp=0.jpg', 'https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=2069030465,1234504942&fm=26&gp=0.jpg','https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=776257843,1161785733&fm=27&gp=0.jpg']
-    },
-    {
-      id: 2,
-      name: '2-2-2',
-      url: ['https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=776257843,1161785733&fm=27&gp=0.jpg', 'https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=2069030465,1234504942&fm=26&gp=0.jpg', 'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=776257843,1161785733&fm=27&gp=0.jpg']
-    },
-    {
-      id: 3,
-      name: '3-3-3',
-      url: ['https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=776257843,1161785733&fm=27&gp=0.jpg', 'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=776257843,1161785733&fm=27&gp=0.jpg', 'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=776257843,1161785733&fm=27&gp=0.jpg']
-    },
-    {
-      id: 4,
-      name: '4-4-4',
-      url: ['https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=776257843,1161785733&fm=27&gp=0.jpg', 'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=776257843,1161785733&fm=27&gp=0.jpg', 'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=776257843,1161785733&fm=27&gp=0.jpg']
-    },
-      {
-        id: 1,
-        name: '1-1-1',
-        url: ['https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=776257843,1161785733&fm=27&gp=0.jpg', 'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=776257843,1161785733&fm=27&gp=0.jpg', 'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=776257843,1161785733&fm=27&gp=0.jpg']
-      },
-      {
-        id: 2,
-        name: '2-2-2',
-        url: ['https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=776257843,1161785733&fm=27&gp=0.jpg', 'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=776257843,1161785733&fm=27&gp=0.jpg', 'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=776257843,1161785733&fm=27&gp=0.jpg']
-      },
-      {
-        id: 3,
-        name: '3-3-3',
-        url: ['https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=776257843,1161785733&fm=27&gp=0.jpg', 'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=776257843,1161785733&fm=27&gp=0.jpg', 'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=776257843,1161785733&fm=27&gp=0.jpg']
-      },
-      {
-        id: 4,
-        name: '4-4-4',
-        url: ['https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=776257843,1161785733&fm=27&gp=0.jpg', 'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=776257843,1161785733&fm=27&gp=0.jpg', 'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=776257843,1161785733&fm=27&gp=0.jpg']
-      },
-    {
-      id: 1,
-      name: '1-1-1',
-      url: ['https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=776257843,1161785733&fm=27&gp=0.jpg', 'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=776257843,1161785733&fm=27&gp=0.jpg', 'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=776257843,1161785733&fm=27&gp=0.jpg']
-    },
-    {
-      id: 2,
-      name: '2-2-2',
-      url: ['https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=776257843,1161785733&fm=27&gp=0.jpg', 'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=776257843,1161785733&fm=27&gp=0.jpg', 'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=776257843,1161785733&fm=27&gp=0.jpg']
-    },
-    {
-      id: 3,
-      name: '3-3-3',
-      url: ['https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=776257843,1161785733&fm=27&gp=0.jpg', 'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=776257843,1161785733&fm=27&gp=0.jpg', 'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=776257843,1161785733&fm=27&gp=0.jpg']
-    },
-    {
-      id: 4,
-      name: '4-4-4',
-      url: ['https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=776257843,1161785733&fm=27&gp=0.jpg', 'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=776257843,1161785733&fm=27&gp=0.jpg', 'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=776257843,1161785733&fm=27&gp=0.jpg']
-    },
-    {
-      id: 1,
-      name: '1-1-1',
-      url: ['https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=776257843,1161785733&fm=27&gp=0.jpg', 'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=776257843,1161785733&fm=27&gp=0.jpg', 'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=776257843,1161785733&fm=27&gp=0.jpg']
-    },
-    {
-      id: 2,
-      name: '2-2-2',
-      url: ['https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=776257843,1161785733&fm=27&gp=0.jpg', 'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=776257843,1161785733&fm=27&gp=0.jpg', 'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=776257843,1161785733&fm=27&gp=0.jpg']
-    },
-    {
-      id: 3,
-      name: '3-3-3',
-      url: ['https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=776257843,1161785733&fm=27&gp=0.jpg', 'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=776257843,1161785733&fm=27&gp=0.jpg', 'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=776257843,1161785733&fm=27&gp=0.jpg']
-    },
-    {
-      id: 4,
-      name: '4-4-4',
-      url: ['https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=776257843,1161785733&fm=27&gp=0.jpg', 'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=776257843,1161785733&fm=27&gp=0.jpg', 'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=776257843,1161785733&fm=27&gp=0.jpg']
-    },
+    mydata: [
+
     ]
   },
 
-  itemClick: function(e){
+  upClick: function(e) {
+    var filter2 = this.data.filter
+    console.log('====', filter2);
+    if (filter2.clothType !== 1) {
+      filter2.clothType = 1
+    }
+    this.setData({
+      filter: filter2
+    })
+  },
+  downClick: function(e) {
+    var filter2 = this.data.filter
+    console.log('====', filter2);
+    if (filter2.clothType !== 2) {
+      filter2.clothType = 2
+    }
+
+    this.setData({
+      filter: filter2
+    })
+  },
+  outherClick: function(e) {
+    var filter2 = this.data.filter
+    console.log('====', filter2);
+    if (filter2.clothType !== 3) {
+      filter2.clothType = 3
+    }
+    this.setData({
+      filter: filter2
+    })
+  },
+  springClick: function(e) {
+    var filter2 = this.data.filter
+    console.log('====', filter2);
+    if (filter2.season !== 1) {
+      filter2.season = 1
+    }
+
+    var source = datasource.getUpSpringData()
+    // 配置banner数据
+    var ban = this.data.bannerdata
+    ban.urls = source[0].url
+    console.log('==== source', ban);
+    this.setData({
+      filter: filter2,
+      bannerdata: ban,
+      mydata: source
+    })
+
+  },
+  summerClick: function(e) {
+    var filter2 = this.data.filter
+    console.log('====', filter2);
+    if (filter2.season !== 2) {
+      filter2.season = 2
+    }
+
+    var source = datasource.getUpSummerData()
+    // 配置banner数据
+    var ban = this.data.bannerdata
+    ban.urls = source[0].url
+    console.log('==== source', ban);
+    this.setData({
+      filter: filter2,
+      bannerdata: ban,
+      mydata: source
+    })
+  },
+  autumnClick: function(e) {
+    var filter2 = this.data.filter
+    console.log('====', filter2);
+    if (filter2.season !== 3) {
+      filter2.season = 3
+    }
+    this.setData({
+      filter: filter2
+    })
+  },
+  winterClick: function(e) {
+    var filter2 = this.data.filter
+    console.log('====', filter2);
+    if (filter2.season !== 4) {
+      filter2.season = 4
+    }
+    this.setData({
+      filter: filter2
+    })
+  },
+
+  itemClick: function(e) {
     var item = e.currentTarget.dataset.id;
     var ban = this.data.bannerdata
     ban.name = item.name
     ban.urls = []
-    for (let i = 0; i < item.url.length; i++){
-      ban.urls.push(item.url[i]) 
+    for (let i = 0; i < item.url.length; i++) {
+      ban.urls.push(item.url[i])
     }
-    this.setData({ bannerdata: ban})
+    this.setData({
+      bannerdata: ban
+    })
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
+    var urls = datasource.getBannerData()
+    var source = datasource.getUpSpringData()
+    // 配置banner数据
     var ban = this.data.bannerdata
-    ban.name = 'asdfasfdasfasfasfsf'
-    
-    var array = this.data.arr
-    for (let i = 1; i < 4; i++) {
-      array.push('https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=776257843,1161785733&fm=27&gp=0.jpg')
-      ban.urls.push('https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=776257843,1161785733&fm=27&gp=0.jpg')
-    }
-    console.log('====',ban);
-    this.setData({ bannerdata: ban})
-    this.setData({ arr: array })
+
+    // var temp = ban.urls.concat(urls)
+
+    ban.urls = urls
+
+    // for(let i=0;i<urls.length;i++){
+    //   ban.urls.push(urls[i])
+    // }
+
+    console.log('==== source', source);
+    this.setData({
+      bannerdata: ban,
+      mydata: source
+    })
 
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
---
+++ clothfashion/utils/data.js
@@ -0,0 +1,76 @@
+function getBannerData(){
+  var banner = ['https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=776257843,1161785733&fm=27&gp=0.jpg', 'https://hbimg.huabanimg.com/c1e2e3014792c36b3f6274cddded6b3a50071cbb1f4cd-UDVjaZ_fw658', 'http://img.zcool.cn/community/010fdd58d8847ea801219c774c5fd7.jpg']
+  return banner
+}
+
+function getUpSpringData(){
+  var source = []
+  var item = {
+    id: 1,
+    name: 2,
+    url: ['https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=776257843,1161785733&fm=27&gp=0.jpg', 'https://hbimg.huabanimg.com/c1e2e3014792c36b3f6274cddded6b3a50071cbb1f4cd-UDVjaZ_fw658', 'http://img.zcool.cn/community/010fdd58d8847ea801219c774c5fd7.jpg']
+  }
+  var item2 = {
+    id: 1,
+    name: 2,
+    url: ['http://img.zcool.cn/community/010fdd58d8847ea801219c774c5fd7.jpg', 'https://hbimg.huabanimg.com/c1e2e3014792c36b3f6274cddded6b3a50071cbb1f4cd-UDVjaZ_fw658', 'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=776257843,1161785733&fm=27&gp=0.jpg']
+  }
+  source.push(item2)
+  source.push(item)
+  return source
+}
+function getUpSummerData() {
+  var source = []
+  var item = {
+    id: 1,
+    name: 2,
+    url: ['https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=776257843,1161785733&fm=27&gp=0.jpg', 'https://hbimg.huabanimg.com/c1e2e3014792c36b3f6274cddded6b3a50071cbb1f4cd-UDVjaZ_fw658', 'http://img.zcool.cn/community/010fdd58d8847ea801219c774c5fd7.jpg']
+  }
+  var item2 = {
+    id: 1,
+    name: 2,
+    url: ['http://img.zcool.cn/community/010fdd58d8847ea801219c774c5fd7.jpg', 'https://hbimg.huabanimg.com/c1e2e3014792c36b3f6274cddded6b3a50071cbb1f4cd-UDVjaZ_fw658', 'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=776257843,1161785733&fm=27&gp=0.jpg']
+  }
+  source.push(item)
+  source.push(item2)
+  return source
+}
+function getUpautumnData() {
+  var source = []
+  var item = {
+    id: 1,
+    name: 2,
+    url: ['https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=776257843,1161785733&fm=27&gp=0.jpg', 'https://hbimg.huabanimg.com/c1e2e3014792c36b3f6274cddded6b3a50071cbb1f4cd-UDVjaZ_fw658', 'http://img.zcool.cn/community/010fdd58d8847ea801219c774c5fd7.jpg']
+  }
+  var item2 = {
+    id: 1,
+    name: 2,
+    url: ['http://img.zcool.cn/community/010fdd58d8847ea801219c774c5fd7.jpg', 'https://hbimg.huabanimg.com/c1e2e3014792c36b3f6274cddded6b3a50071cbb1f4cd-UDVjaZ_fw658', 'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=776257843,1161785733&fm=27&gp=0.jpg']
+  }
+  source.push(item2)
+  source.push(item)
+  return source
+}
+function getUpWinterData() {
+  var source = []
+  var item = {
+    id: 1,
+    name: 2,
+    url: ['https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=776257843,1161785733&fm=27&gp=0.jpg', 'https://hbimg.huabanimg.com/c1e2e3014792c36b3f6274cddded6b3a50071cbb1f4cd-UDVjaZ_fw658', 'http://img.zcool.cn/community/010fdd58d8847ea801219c774c5fd7.jpg']
+  }
+  var item2 = {
+    id: 1,
+    name: 2,
+    url: ['http://img.zcool.cn/community/010fdd58d8847ea801219c774c5fd7.jpg', 'https://hbimg.huabanimg.com/c1e2e3014792c36b3f6274cddded6b3a50071cbb1f4cd-UDVjaZ_fw658', 'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=776257843,1161785733&fm=27&gp=0.jpg']
+  }
+  source.push(item)
+  source.push(item2)
+  return source
+}
+module.exports = {
+  getBannerData : getBannerData,
+  getUpSpringData: getUpSpringData,
+  getUpSummerData: getUpSummerData,
+  getUpautumnData: getUpautumnData,
+  getUpWinterData: getUpWinterData
+}
